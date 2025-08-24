2375d2dd2def0250526f2aa8b73ec35a65ec4765
midrisR <midrisramdhani96@gmail.com>
2025-08-24 18:19:05 +0700

    error subtitles 2

--- app/example/page.jsx
+++ app/example/page.jsx
@@ -1,20 +1,21 @@
 // app/watch/page.jsx
-import Player from "@/components/player";
-import Link from "next/link";
+// import Player from "@/components/player";
+// import Link from "next/link";
 
 export default async function Page() {
-  const response = await fetch(`${process.env.API_URL}/api/top-ten`);
-  const { results } = await response.json();
-  const url = (text) => {
-    return text
-      .toLowerCase() // jadi huruf kecil semua
-      .replace(/[^a-z0-9\s-]/g, "") // hapus semua karakter selain huruf, angka, spasi, dan "-"
-      .trim() // hapus spasi di awal/akhir
-      .replace(/\s+/g, "-"); // ganti spasi jadi "-"
-  };
+  // const response = await fetch(`${process.env.API_URL}/api/top-ten`);
+  // const { results } = await response.json();
+  // const url = (text) => {
+  //   return text
+  //     .toLowerCase() // jadi huruf kecil semua
+  //     .replace(/[^a-z0-9\s-]/g, "") // hapus semua karakter selain huruf, angka, spasi, dan "-"
+  //     .trim() // hapus spasi di awal/akhir
+  //     .replace(/\s+/g, "-"); // ganti spasi jadi "-"
+  // };
   return (
     <main className="container mx-auto p-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+      <h1>example</h1>
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
         {results.month.map((card) => (
           <Link href={`/example/${url(card.id)}`} key={card.id}>
             <div className="flex flex-col justify-center w-3xs mx-auto mb-6">
@@ -30,7 +31,7 @@ export default async function Page() {
             </div>
           </Link>
         ))}
-      </div>
+      </div> */}
     </main>
   );
 }
